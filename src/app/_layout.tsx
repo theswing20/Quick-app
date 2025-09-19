2162274e259d9f4d23796077690667fb83c1ba0f
Zhalgas <theswing20@gmail.com>
2025-09-19 14:49:09 +0500

    Implement theme

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,11 +1,26 @@
+import { NAV_THEME } from "@/shared/lib/theme";
 import { PortalHost } from "@rn-primitives/portal";
 import { Stack } from "expo-router";
+import { useColorScheme } from "react-native";
 import "../../global.css";
 
 export default function RootLayout() {
+  const colorScheme = useColorScheme();
+
   return (
     <>
-      <Stack />
+      <Stack
+        screenOptions={{
+          headerStyle: {
+            backgroundColor:
+              NAV_THEME[colorScheme ?? "light"].colors.background,
+          },
+          headerTintColor: NAV_THEME[colorScheme ?? "light"].colors.text,
+          headerTitleStyle: {
+            color: NAV_THEME[colorScheme ?? "light"].colors.text,
+          },
+        }}
+      />
       <PortalHost />
     </>
   );
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -4,8 +4,8 @@ import { View } from "react-native";
 
 export default function Index() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-xl font-bold text-blue-500">
+    <View className="flex-1 items-center justify-center bg-background">
+      <Text className="text-xl font-bold text-foreground">
         Welcome to Nativewind!
       </Text>
       <Button>
