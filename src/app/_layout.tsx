ff7b6668febc0a91edf0f93f29e9171f55fe440c
Zhalgas <theswing20@gmail.com>
2025-09-19 14:36:12 +0500

    Implement reusbales UI

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,5 +1,12 @@
+import { PortalHost } from "@rn-primitives/portal";
 import { Stack } from "expo-router";
+import "../../global.css";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <>
+      <Stack />
+      <PortalHost />
+    </>
+  );
 }
